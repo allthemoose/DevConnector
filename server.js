45e50d8ff301c13010f4da2166781894ddcaa29c
allthemoose <andrewjwhitehead84@gmail.com>
2020-07-26 01:47:43 +1200

    Section 3

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
 
+connectDB();
 app.get('/', (req, res) => res.send('API Running'));
+
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
