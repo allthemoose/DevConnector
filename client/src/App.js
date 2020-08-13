d812fd25a757fae4ce79a32d24faf911fe2d7e61
allthemoose <andrewjwhitehead84@gmail.com>
2020-08-13 23:52:22 +1200

    First reducers and actions

--- client/src/App.js
+++ client/src/App.js
@@ -10,24 +10,32 @@ import Navbar from './Components/layout/Navbar';
 import Landing from './Components/layout/Landing';
 import Register from './Components/auth/Register';
 import Login from './Components/auth/Login';
+import Alert from './Components/layout/Alert';
+
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route
-            exact
-            path='/Register'
-            component={Register}
-          />
-          <Route exact path='/Login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Alert></Alert>
+          <Switch>
+            <Route
+              exact
+              path='/Register'
+              component={Register}
+            />
+            <Route exact path='/Login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
