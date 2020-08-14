c486d2b1f78ce60096935cc59f0c039f0f4b784b
allthemoose <andrewjwhitehead84@gmail.com>
2020-08-14 23:43:01 +1200

    Starting auth action

--- client/src/Components/auth/Register.js
+++ client/src/Components/auth/Register.js
@@ -2,9 +2,11 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+
 import PropTypes from 'prop-types';
 
-export const Register = ({ setAlert }) => {
+export const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -24,9 +26,10 @@ export const Register = ({ setAlert }) => {
     e.preventDefault();
     if (password !== password2) {
       setAlert('Passwords Do Not Match', 'danger');
+    } else {
+      register({ name, email, password });
+      return e;
     }
-    console.log(formData);
-    return e;
   };
 
   return (
@@ -95,6 +98,9 @@ export const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(
+  Register
+);
---
+++ client/src/reducers/auth.js
@@ -0,0 +1,37 @@
+// Auth reducer - process registration, logins, tokens and authentication
+
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+} from '../actions/types';
+
+initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: null,
+  loading: true,
+  user: null,
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case REGISTER_SUCCESS:
+      localStorage.setItem('token', payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAthenticated: true,
+        loading: false,
+      };
+    case REGISTER_FAIL:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAthenticated: false,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
