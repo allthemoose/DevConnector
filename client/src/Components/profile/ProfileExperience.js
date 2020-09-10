fdf4c13df39952ead4cb8f1b7d5b6a9c6c21ee5f
allthemoose <andrewjwhitehead84@gmail.com>
2020-09-10 21:15:25 +1200

    Profile Education & Experience
    Profile View component includes Experience and Education

---
+++ client/src/Components/profile/ProfileExperience.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const ProfileExperience = ({
+  experience: { title, company, location, from, current, to, description },
+}) => (
+  <div>
+    <h3 class='text-dark'>{company}</h3>
+    <p>
+      <Moment format='YYYY-MM-DD'>{from}</Moment> -{' '}
+      {!to ? 'Now' : <Moment format='YYYY-MM-DD'>{to}</Moment>}
+    </p>
+    <p>
+      <strong>Position: </strong>
+      {title}
+    </p>
+    <p>
+      {description && (
+        <Fragment>
+          <strong>Description: </strong>
+          {description}
+        </Fragment>
+      )}
+    </p>
+  </div>
+);
+
+ProfileExperience.propTypes = {
+  experience: PropTypes.object.isRequired,
+};
+
+export default ProfileExperience;
